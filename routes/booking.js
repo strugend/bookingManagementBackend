28155056edb7ea58429ca51c1e5a208240f2014c
triyam <triyambakam2@gmail.com>
2023-03-26 16:22:04 +0530

    Booked time and booked date added

--- routes/booking.js
+++ routes/booking.js
@@ -37,6 +37,7 @@ router.get("/getAppointment", async (req, res) => {
     }
 });
 
+
 router.post("/scheduleAppointment", async (req, res) => {
     const { timeSchedule, dateSchedule, question_name, question_email, question_phone, question_address, question_numberOfPeople } = req.body;
     const booking_link_id = req.query.booking_link_id
@@ -44,22 +45,30 @@ router.post("/scheduleAppointment", async (req, res) => {
         return res.status(422).json({ error: "Some data fields are missing" });
     }
     try {
-        bookingId = await User.find({ booking_link_id: booking_link_id })
-        bookedtimeSchedule = await BookingUser.find({ timeSchedule: timeSchedule })
-        bookeddateSchedule = await BookingUser.find({ dateSchedule: dateSchedule })
+        User.findOne({ booking_link_id }, async (err, result) => {
+            if (err) throw err;
+            if (!result) {
+                res.status(422).json({ message: "Error: Booking ID not found in database." })
+                return;
+            } else {
+                const book = new BookingUser({
+                    booking_link_id: booking_link_id,
+                    timeSchedule,
+                    dateSchedule,
+                    question_name,
+                    question_email,
+                    question_phone,
+                    question_address,
+                    question_numberOfPeople
+                });
+                await book.save();
+                res.status(201).json({ message: "Booking Done Successfully" });
 
-            const book = new BookingUser({
-                booking_link_id: booking_link_id,
-                timeSchedule,
-                dateSchedule,
-                question_name,
-                question_email,
-                question_phone,
-                question_address,
-                question_numberOfPeople
-            });
-            await book.save();
-            res.status(201).json({ message: "Booking Done Successfully" });
+            }
+        })
+        // bookingId = await User.findOne({ booking_link_id: booking_link_id })
+        // console.log(bookingId);
+
 
     } catch (error) {
         res.status(500).json({ message: "Internal Server Error" });
@@ -67,6 +76,58 @@ router.post("/scheduleAppointment", async (req, res) => {
     }
 })
 
+
+router.get("/bookedAppointmentTime", async (req, res) => {
+    const booking_link_id = req.query.booking_link_id
+    if (!booking_link_id) {
+        return res.status(422).json({ error: "Some data fields are missing" });
+    }
+    try {
+        BookingUser.find({ booking_link_id }, async (err, result) => {
+            if (err) throw err;
+            if (!result) {
+                res.status(401).json({ message: "Error: Booking ID not found in database." })
+                return;
+            } else {
+                let bookedTime = []
+                for(i=0; i<result.length; i++) {
+                    bookedTime.push(result[i]["timeSchedule"]);
+                }
+                res.status(201).json({ bookedTime: bookedTime });
+        }
+        })
+    } catch (error) {
+        res.status(500).json({ message: "Internal Server Error" });
+        console.log(error);
+    }
+
+})
+
+router.get("/bookedAppointmentDate", async (req, res) => {
+    const booking_link_id = req.query.booking_link_id
+    if (!booking_link_id) {
+        return res.status(422).json({ error: "Some data fields are missing" });
+    }
+    try {
+        BookingUser.find({ booking_link_id }, async (err, result) => {
+            if (err) throw err;
+            if (!result) {
+                res.status(401).json({ message: "Error: Booking ID not found in database." })
+                return;
+            } else {
+                let bookedDate = []
+                for(i=0; i<result.length; i++) {
+                    bookedDate.push(result[i]["dateSchedule"]);
+                }
+                res.status(201).json({ bookedDate: bookedDate });
+        }
+        })
+    } catch (error) {
+        res.status(500).json({ message: "Internal Server Error" });
+        console.log(error);
+    }
+
+})
 
 // router.post("/updateAppointment", async (req, res) => {
 //     try {
